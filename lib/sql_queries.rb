aeb0bcc7fc4372aae78d1ac6df83a6e387da14fe
Monica Marziano <mmarziano1@gmail.com>
2019-01-02 13:21:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT Users.name, SUM(Pledges.amount) FROM users JOIN pledges ON Users.id = Pledges.user_id ORDER BY Users.name ASC"
+"SELECT Users.name, SUM(Pledges.amount) as total_pledged FROM users JOIN pledges ON Users.id = Pledges.user_id ORDER BY total_pledged ASC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
