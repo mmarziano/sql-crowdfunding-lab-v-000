b8ede8e691ac124c2830e05fbbdfbf50efca1b4f
Monica Marziano <mmarziano1@gmail.com>
2019-01-02 13:29:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+  "SELECT Projects.category, SUM(Pledges.amount) FROM projects JOIN pledges ON Projects.id = Pledges.project_id WHERE Projects.category = GROUP BY Projects.category ORDER BY total_pledged ASC"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
